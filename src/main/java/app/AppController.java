b5794f36b02ef341244dbf6fec0af7f1809713f3
renovaldes <renovaldes@email.arizona.edu>
2018-04-22 13:57:50 -0700

    Added controller.

--- src/main/java/app/AppController.java
+++ src/main/java/app/AppController.java
@@ -1,5 +1,6 @@
 package app;
 
+import models.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -28,8 +30,20 @@ public class AppController {
     }
 
     @GetMapping("/students")
-    public String greeting() {
-
+    public Student greeting() {
+        Student student = new Student();
+        student.setID(27);
+        student.setName("Reno");
+        student.setAddress("Address");
+        student.setPhone("520-909-0123");
+        student.setEmail("email@email.com");
+        student.setGender('M');
+        student.setDob(new Date());
+        student.setCategory("Cat");
+        student.setMajor("Computer Science");
+        student.setMinor("ISTA");
+        student.setAdvisorID(10);
+        return student;
     }
 
 }
--- src/main/java/models/Student.java
+++ src/main/java/models/Student.java
@@ -1,4 +1,4 @@
-package Models;
+package models;
 import java.util.Date;
 
 public class Student {
