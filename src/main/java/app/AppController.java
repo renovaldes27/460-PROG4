e2dbf900794537b495172e0bc75e87a7239b3494
Reno Donald Valdes <renovaldes@cs.arizona.edu>
2018-04-22 13:42:16 -0700

    Changed project layout.

--- src/main/java/app/AppController.java
+++ src/main/java/app/AppController.java
@@ -1,4 +1,4 @@
-package hello;
+package app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,7 +26,6 @@ public class AppController {
     private void postConstruct() {
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
-
 
     @GetMapping("/students")
     public String greeting() {
