1c0ab51ee47bda5d4b76535a3ed88f07a8075736
renovaldes <renovaldes@email.arizona.edu>
2018-04-22 18:02:53 -0700

    stuff

--- src/main/java/app/AppController.java
+++ src/main/java/app/AppController.java
@@ -10,6 +10,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
