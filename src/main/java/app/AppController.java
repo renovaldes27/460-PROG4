93a3bc57cc2c96fed8fb3d44dbfd28ffa36f658c
renovaldes <renovaldes@email.arizona.edu>
2018-04-22 18:05:29 -0700

    Stuff

--- src/main/java/app/AppController.java
+++ src/main/java/app/AppController.java
@@ -2,36 +2,17 @@ package app;
 
 import models.Student;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Date;
-import java.util.List;
+
 
 @Controller
 public class AppController {
-    /*
-    @Autowired
-    private DataSource dataSource;
-    private JdbcTemplate jdbcTemplate;
 
-    @PostConstruct
-    private void postConstruct() {
-        jdbcTemplate = new JdbcTemplate(dataSource);
-    }
-    */
 
     @RequestMapping(value ="/students", method = RequestMethod.GET)
     @ResponseBody
