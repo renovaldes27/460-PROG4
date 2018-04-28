fbacad5e2781ab4fb083d886d9e7463ea82e8926
Isaac Plunkett <isaacp@email.arizona.edu>
2018-04-28 16:03:15 -0700

    lets try this

--- src/main/java/models/Student.java
+++ src/main/java/models/Student.java
@@ -95,7 +95,7 @@ public class Student
 
         try
         {
-            ResultSet answer = statement.executeQuery("select isaacp.student.id as id, isaacp.student.name as name, address, phoneNumber, email, gender, dob, category, isaacp.department.name as majorid, minorid, AdvisorID" + 
+            ResultSet answer = statement.executeQuery("select isaacp.student.id as id, isaacp.student.name as name, address, phoneNumber, email, gender, dob, category, isaacp.department.name as majorid, isaacp.student.minorid, isaacp.student.AdvisorID" + 
             " from isaacp.student join isaacp.department on (isaacp.student.majorID = isaacp.department.id)" +
             "");
 
