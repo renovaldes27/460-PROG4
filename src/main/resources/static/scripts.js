3ad1b2d8a24f0b3c9b049d96defbee8bd38fd450
renovaldes <renovaldes@email.arizona.edu>
2018-05-01 00:21:24 -0700

    stuff

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -855,5 +855,24 @@ function toggle(elId) {
         tables[i].style.display = 'none';
     }
 
+    $.getJSON( "/summary", function( data ) {
+        document.getElementById("fresh-apt").text = data.freshApt;
+        document.getElementById("fresh-hall").text = data.freshDorm;
+
+        document.getElementById("soph-apt").text = data.sophApt;
+        document.getElementById("soph-hall").text = data.sophDorm;
+
+        document.getElementById("junior-apt").text = data.junApt;
+        document.getElementById("junior-hall").text = data.junDorm;
+
+        document.getElementById("senior-apt").text = data.senApt;
+        document.getElementById("senior-hall").text = data.senDorm;
+
+        document.getElementById("grad-apt").text = data.gradApt;
+        document.getElementById("grad-hall").text = data.gradDorm;
+
+      });
+
     document.getElementById(elId).style.display = 'block';
+
 }
