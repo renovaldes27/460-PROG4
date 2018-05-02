2d9dc951e32701206eb3c88a1dbb4990f9cb6e6a
Isaac Plunkett <isaacp@email.arizona.edu>
2018-05-02 09:33:33 -0700

    removed unnessisary imports

--- src/main/java/app/AppController.java
+++ src/main/java/app/AppController.java
@@ -3,24 +3,12 @@ package app;
 import models.*;
 
 import org.springframework.stereotype.Controller; 
-import org.springframework.ui.Model; 
-import org.springframework.web.bind.annotation.GetMapping; 
 import org.springframework.web.bind.annotation.RequestParam; 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
-
 import org.springframework.web.bind.annotation.*; 
-
-import javax.annotation.PostConstruct; 
 import javax.sql.*; 
-
 import java.sql.*; 
-import java.text.*; 
-import java.util.Date; 
 import java.util.*;
 
 @Controller
